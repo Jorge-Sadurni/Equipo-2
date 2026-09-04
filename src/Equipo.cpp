9161658077d4917cf7e6460f0edf6873cf71a9f6
nestor-lopez-07 <nestor.lopez@lasallesaltillo.edu.mx>
2026-09-04 14:50:43 -0600

    modificacion de equipo.cpp

--- src/Equipo.cpp
+++ src/Equipo.cpp
@@ -1,49 +1,45 @@
 #include "Equipo.h"
 #include <iostream>
 
-using namespace std;
+Equipo::Equipo(const std::string& nombreEquipo) : nombreEquipo(nombreEquipo) {}
 
-Equipo::Equipo(const string &nombreEquipo)
-    : nombreEquipo(nombreEquipo) {}
-
-void Equipo::agregarIntegrante(const Integrante &i)
-{
+void Equipo::agregarIntegrante(const Integrante& i) {
+    if (integrantes.size() >= 3) {
+        throw std::runtime_error("El equipo ya tiene el número máximo de integrantes (3)");
+    }
     integrantes.push_back(i);
 }
 
-void Equipo::agregarRobot(const Robot &r)
-{
+void Equipo::agregarRobot(const Robot& r) {
     robots.push_back(r);
 }
 
-string Equipo::getNombreEquipo() const
-{
+std::string Equipo::getNombreEquipo() const {
     return nombreEquipo;
 }
 
-const vector<Integrante> &Equipo::getIntegrantes() const
-{
+const std::vector<Integrante>& Equipo::getIntegrantes() const {
     return integrantes;
 }
 
-const vector<Robot> &Equipo::getRobots() const
-{
+const std::vector<Robot>& Equipo::getRobots() const {
     return robots;
 }
 
-void Equipo::mostrarResumen() const
-{
-    cout << "\n=== Equipo: " << nombreEquipo << " ===\n";
-
-    cout << "Integrantes (" << integrantes.size() << "):\n";
-    for (const auto &i : integrantes)
-    {
-        cout << "  - " << i.getNombre() << " (" << i.getRol() << ")\n";
+void Equipo::mostrarResumen() const {
+    std::cout << "\n  Equipo: " << nombreEquipo 
+              << " (" << integrantes.size() << " integrantes, " 
+              << robots.size() << " robots)" << std::endl;
+    
+    std::cout << "  Integrantes:" << std::endl;
+    for (const auto& integrante : integrantes) {
+        std::cout << "    - " << integrante.getNombre() 
+                  << " (" << integrante.getRol() << ")" << std::endl;
     }
-
-    cout << "Robots (" << robots.size() << "):\n";
-    for (const auto &r : robots)
-    {
-        cout << "  - " << r.getNombre() << " [" << r.getTipo() << "]\n";
+    
+    std::cout << "  Robots:" << std::endl;
+    for (const auto& robot : robots) {
+        std::cout << "    - " << robot.getNombre() 
+                  << " (" << robot.getTipo() << ")" << std::endl;
     }
 }
